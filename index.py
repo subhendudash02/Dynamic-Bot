843a599d06e6cd0a8f743e7f0692508a4d68fc94
Subhendu Dash <subhendu.dash2020@vitstudent.ac.in>
2022-01-21 10:24:33 +0530

    Enhanced help command

--- index.py
+++ index.py
@@ -7,10 +7,10 @@ import os
 import pytz
 import nacl
 
-#client = discord.Client()
+# client = discord.Client()
 bot = commands.Bot(command_prefix = "!")
 
-bot.remove_command("help")
+# bot.remove_command("help")
 
 region = pytz.timezone("Asia/Kolkata")
 
@@ -20,18 +20,18 @@ async def on_ready():
     print("Dynamic Bot is now online.")
 """
 
-@bot.command()
+@bot.command(name='desc', help='About Me 😁')
 async def desc(ctx):
     await ctx.send("Hey there! I am a Dynamic Bot. If you wanna set a reminder, Dynamic Bot is there for you!\nPeace!!✌")
 
-@bot.command()
-async def help(ctx):
-    await ctx.send("**Commands by Dynamic-Bot: ** \n)
-    await ctx.send("1. `!desc` : About Me 😁 \n")
-    await ctx.send("2. `!time` : Ask the current time \n")
-    await ctx.send("3. `!reminder <date: dd/mm/yyyy> <time: hh:mm> <event_name>` : Set a reminder of any Event \n")
-    await ctx.send("4. `!join` : Join the discord server (make sure you join first) \n")
-    await ctx.send("5. `!leave` : Leave the discord server")
+# @bot.command()
+# async def help(ctx):
+#     await ctx.send("**Commands by Dynamic-Bot: ** \n")
+#     await ctx.send("1. `!desc` : About Me 😁 \n")
+#     await ctx.send("2. `!time` : Ask the current time \n")
+#     await ctx.send("3. `!reminder <date: dd/mm/yyyy> <time: hh:mm> <event_name>` : Set a reminder of any Event \n")
+#     await ctx.send("4. `!join` : Join the discord server (make sure you join first) \n")
+#     await ctx.send("5. `!leave` : Leave the discord server")
 
 """
 bot.command()
@@ -40,13 +40,13 @@ async def info(ctx):
     await ctx.send(ctx.author)
 """
 
-@bot.command()
+@bot.command(name='!time', help='Ask the current time')
 async def time(ctx):
     TIME = datetime.now(region)
     x = TIME.strftime("%H:%M:%S")
     await ctx.send(f"Hey, {ctx.author}! The time is {x}")
 
-@bot.command()
+@bot.command(name='!reminder', help='Format: `<date: dd/mm/yyyy> <time: hh:mm> <event_name>`')
 async def reminder(ctx, date: str,Time: str, *, msg):
     time_format = "%d/%m/%Y %H:%M"
     upcoming_time = date + " " + Time
@@ -66,7 +66,7 @@ async def reminder(ctx, date: str,Time: str, *, msg):
         await ctx.send(f'Buckle up @everyone! {msg} is coming up!! Be prepared...')
         break
 
-@bot.command()
+@bot.command(name='!join', help='Join the discord server (make sure you join first)')
 async def join(ctx):
     vc = ctx.author.voice
 
@@ -76,11 +76,10 @@ async def join(ctx):
         await ctx.send("Joined!!!")
         await vc.channel.connect()
 
-@bot.command()
+@bot.command(name='!leave', help='Leave the discord server')
 async def leave(ctx):
     vc_leave = ctx.voice_client
     await vc_leave.disconnect()
     await ctx.send("Left!!!")
 
 bot.run(os.environ["TOKEN"])
-# client.run(token)
